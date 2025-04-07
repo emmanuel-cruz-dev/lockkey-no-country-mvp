af183982cc20fa11c5d362bd8210ae664eca84cf
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-04-07 11:50:04 -0300

    style: estilos adaptados en WhoWeAre section

--- src/Frontend/pages/Home/WhoWeAre/CounterItem.tsx
+++ src/Frontend/pages/Home/WhoWeAre/CounterItem.tsx
@@ -1,14 +1,19 @@
 import { FC } from "react";
 import { CounterItemProps } from "../../../Store/types";
+import { useDarkMode } from "../../../hooks/useDarkMode";
 
 export const CounterItem: FC<CounterItemProps> = ({ count, text }) => {
+  const { isDarkMode } = useDarkMode();
+
   return (
     <div>
       <h3 className="text__purple-dark text-4xl font-semibold mb-2">
         {count}
         <span className="text-2xl align-top">+</span>
       </h3>
-      <p>{text}</p>
+      <p className={`${isDarkMode ? "text-white/60" : "text-neutral-700"}`}>
+        {text}
+      </p>
     </div>
   );
 };
--- src/Frontend/pages/Home/WhoWeAre/WhoWeAre.tsx
+++ src/Frontend/pages/Home/WhoWeAre/WhoWeAre.tsx
@@ -1,20 +1,33 @@
 import WhoWeAreImg from "../../../../assets/who-we-are-banner.avif";
+import { useDarkMode } from "../../../hooks/useDarkMode";
 import { CounterItem } from "./CounterItem";
 
 function WhoWeAre() {
+  const { isDarkMode } = useDarkMode();
+
   return (
     <section
       className="container who-we-are grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-2 py-12"
       id="who-we-are"
     >
       <article className="order-2 md:order-1 text-center md:text-left">
-        <p className="text-lg uppercase mb-2">Quiénes somos</p>
+        <p
+          className={`${
+            isDarkMode ? "text-white/80" : "text-neutral-800"
+          } text-lg uppercase mb-2`}
+        >
+          Quiénes somos
+        </p>
         <h2 className="text-3xl lg:text-4xl xl:text-5xl font-bold mb-5">
           Protegiendo{" "}
           <span className="text__purple-dark">tu seguridad digital</span> con
           dedicación, tecnología y confianza
         </h2>
-        <p className="text-lg mb-5">
+        <p
+          className={`${
+            isDarkMode ? "text-white/70" : "text-neutral-700"
+          } text-lg mb-5`}
+        >
           Somos un equipo comprometido con la ciberseguridad, ofreciendo
           soluciones innovadoras para proteger información valiosa. Nuestra
           misión es garantizar la tranquilidad digital de usuarios y empresas
