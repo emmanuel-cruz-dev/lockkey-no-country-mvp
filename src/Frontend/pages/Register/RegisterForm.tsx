09625d5f06a5d4fe09b61990b8c2d5f61f0592a6
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-04-10 11:56:30 -0300

    refactor: RegisterForm creado para dividir responsabilidades en Register.tsx

---
+++ src/Frontend/pages/Register/RegisterForm.tsx
@@ -0,0 +1,183 @@
+import { Link } from "react-router-dom";
+import { useDarkMode } from "../../hooks/useDarkMode";
+import useRegister from "../../hooks/useRegister";
+
+function RegisterForm() {
+  const { isDarkMode } = useDarkMode();
+  const {
+    state,
+    showPassword,
+    passwordScore,
+    suggestions,
+    emailError,
+    timeToCrack,
+    apiError,
+    togglePasswordVisibility,
+    handleChange,
+    handleSubmit,
+    strengthColor,
+  } = useRegister();
+
+  return (
+    <form onSubmit={handleSubmit} className="space-y-4 text-black">
+      <div>
+        <label
+          className={`block text-sm font-medium ${
+            isDarkMode ? "text-white/80" : "text-gray-700"
+          }`}
+        >
+          Correo electrónico
+        </label>
+        <input
+          type="email"
+          name="email"
+          value={state.email}
+          onChange={handleChange}
+          className="mt-1 w-full rounded-md border px-3 py-2"
+          required
+        />
+        {emailError && (
+          <p className="text-red-500 text-sm mt-1">{emailError}</p>
+        )}
+      </div>
+      <div>
+        <label
+          className={`block text-sm font-medium ${
+            isDarkMode ? "text-white/80" : "text-gray-700"
+          }`}
+        >
+          Contraseña Maestra
+        </label>
+        <div className="relative">
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            value={state.password}
+            onChange={handleChange}
+            className="mt-1 w-full rounded-md border px-3 py-2"
+            required
+          />
+          <button
+            type="button"
+            onClick={togglePasswordVisibility}
+            className="absolute inset-y-0 right-0 px-3 flex items-center text-gray-500 hover:text-gray-700 text-sm"
+          >
+            {showPassword ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                />
+              </svg>
+            )}
+          </button>
+        </div>
+      </div>
+
+      <div className="w-full h-2 mt-2 rounded bg-gray-300">
+        <div
+          className={`h-2 ${strengthColor} rounded`}
+          style={{ width: `${(passwordScore + 1) * 20}%` }}
+        ></div>
+      </div>
+      {timeToCrack && (
+        <p
+          className={`${
+            isDarkMode ? "text-white/60" : "text-gray-600"
+          } text-sm mt-1`}
+        >
+          ⏳ Tiempo estimado para descifrar: <strong>{timeToCrack}</strong>
+        </p>
+      )}
+      {suggestions.length > 0 && (
+        <ul
+          className={`mt-2 text-sm ${
+            isDarkMode ? "text-white/60" : "text-gray-600"
+          }`}
+        >
+          {suggestions.map((s, i) => (
+            <li key={i}>• {s}</li>
+          ))}
+        </ul>
+      )}
+      {apiError && <p className="text-red-500 text-sm mt-1">{apiError}</p>}
+
+      <div>
+        <label
+          className={`block text-sm font-medium ${
+            isDarkMode ? "text-white/80" : "text-gray-700"
+          }`}
+        >
+          Confirmar contraseña Maestra
+        </label>
+        <input
+          type="password"
+          name="confirmPassword"
+          value={state.confirmPassword}
+          onChange={handleChange}
+          className="mt-1 w-full rounded-md border px-3 py-2"
+          required
+        />
+      </div>
+      <p
+        className={`mt-4 text-center text-sm ${
+          isDarkMode ? "text-white/60" : "text-gray-600"
+        }`}
+      >
+        ¿Ya tienes una cuenta?
+        <Link
+          to="/login"
+          className={`${
+            isDarkMode ? "text-blue-400" : "text-blue-600"
+          } hover:underline`}
+        >
+          {" "}
+          Inicia sesión aquí
+        </Link>
+      </p>
+      <button
+        type="submit"
+        className="btn__primary btn__lime w-full text-black cursor-pointer"
+        disabled={!state.password || passwordScore < 3}
+      >
+        Regístrate
+      </button>
+      {passwordScore < 3 && (
+        <p className="text-red-500 text-sm mt-1">
+          La contraseña es demasiado débil. Usa una más segura.
+        </p>
+      )}
+    </form>
+  );
+}
+
+export default RegisterForm;
