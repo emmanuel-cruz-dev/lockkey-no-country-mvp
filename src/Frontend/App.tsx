38297250b13ae7e2bc2c3e12bd9b6ecf1f27137e
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-04-11 09:35:55 -0300

    refactor: AppRouter creado para manejar el enrutado general

--- src/Frontend/App.tsx
+++ src/Frontend/App.tsx
@@ -1,42 +1,18 @@
-import { lazy } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Header from "./layouts/Header/Header";
 import Footer from "./layouts/Footer/Footer";
 import ScrollToTopButton from "./components/ScrollToTopButton/ScrollToTopButton";
 import { ModalProvider } from "./providers/ModalProvider";
-import Home from "./routes/Home";
-import About from "./routes/About";
-import Services from "./routes/Services";
-import ContactPage from "./routes/ContactPage";
 import ModalContainer from "./components/ModalContainer/ModalContainer";
-
-const Register = lazy(() => import("./pages/Register/Register"));
-const Login = lazy(() => import("./pages/Login/Login"));
-const Panel = lazy(() => import("./pages/Panel/Panel"));
+import AppRouter from "./routes/AppRouter";
 
 function App() {
-  //const isAuthenticated = !!localStorage.getItem("access_token"); // Agregar esta línea al finalizar
-  const isAuthenticated = true; // No olvidar borrar esta línea
-
   return (
     <ModalProvider>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/panel"
-          element={isAuthenticated ? <Panel /> : <Navigate to="/login" />}
-        />
-      </Routes>
+      <AppRouter />
       <ScrollToTopButton />
       <Footer />
-
       <ModalContainer />
     </ModalProvider>
   );
---
+++ src/Frontend/routes/AppRouter.tsx
@@ -0,0 +1,30 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Services from "./Services";
+import ContactPage from "./ContactPage";
+import Register from "../pages/Register/Register";
+import Login from "../pages/Login/Login";
+import Panel from "../pages/Panel/Panel";
+
+function AppRouter() {
+  // const isAuthenticated = !!localStorage.getItem("access_token"); // Agregar esta línea al finalizar
+  const isAuthenticated = true; // No olvidar borrar esta línea
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/panel"
+        element={isAuthenticated ? <Panel /> : <Navigate to="/login" />}
+      />
+    </Routes>
+  );
+}
+
+export default AppRouter;
