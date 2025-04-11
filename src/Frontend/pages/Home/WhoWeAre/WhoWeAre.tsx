53a40a248731df2a96952f96b31e933eeb28ee1d
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-04-11 11:03:59 -0300

    feat: animaciones agreadas a WhoWeAre

--- src/Frontend/pages/Home/WhoWeAre/WhoWeAre.tsx
+++ src/Frontend/pages/Home/WhoWeAre/WhoWeAre.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import WhoWeAreImg from "../../../../assets/who-we-are-banner.avif";
 import { useDarkMode } from "../../../hooks/useDarkMode";
 import { CounterItem } from "./CounterItem";
+import AnimatedComponent from "../../../components/AnimatedComponent/AnimatedComponent";
 
 function WhoWeAre() {
   const { isDarkMode } = useDarkMode();
@@ -13,53 +14,59 @@ function WhoWeAre() {
       id="who-we-are"
     >
       <article className="order-2 md:order-1 text-center md:text-left">
-        <p
-          className={`${
-            isDarkMode ? "text-white/80" : "text-neutral-800"
-          } text-lg uppercase mb-2`}
-        >
-          {t("pages.home.whoWeAre.paragraph")}
-        </p>
-        <h2 className="text-3xl lg:text-4xl xl:text-5xl font-bold mb-5">
-          {t("pages.home.whoWeAre.title1")}{" "}
-          <span className="text__purple-dark">
-            {t("pages.home.whoWeAre.titleSpan")}
-          </span>{" "}
-          {t("pages.home.whoWeAre.title2")}
-        </h2>
-        <p
-          className={`${
-            isDarkMode ? "text-white/70" : "text-neutral-700"
-          } text-lg mb-5`}
-        >
-          {t("pages.home.whoWeAre.description")}
-        </p>
-        <footer className="flex flex-col md:flex-row gap-8">
-          <CounterItem
-            count="2K"
-            text={t("pages.home.whoWeAre.counterItem1")}
-          />
-          <CounterItem
-            count="17M"
-            text={t("pages.home.whoWeAre.counterItem2")}
-          />
-          <CounterItem
-            count="18K"
-            text={t("pages.home.whoWeAre.counterItem3")}
-          />
-        </footer>
+        <AnimatedComponent animation="slide" direction="right">
+          <p
+            className={`${
+              isDarkMode ? "text-white/80" : "text-neutral-800"
+            } text-lg uppercase mb-2`}
+          >
+            {t("pages.home.whoWeAre.paragraph")}
+          </p>
+          <h2 className="text-3xl lg:text-4xl xl:text-5xl font-bold mb-5">
+            {t("pages.home.whoWeAre.title1")}{" "}
+            <span className="text__purple-dark">
+              {t("pages.home.whoWeAre.titleSpan")}
+            </span>{" "}
+            {t("pages.home.whoWeAre.title2")}
+          </h2>
+          <p
+            className={`${
+              isDarkMode ? "text-white/70" : "text-neutral-700"
+            } text-lg mb-5`}
+          >
+            {t("pages.home.whoWeAre.description")}
+          </p>
+        </AnimatedComponent>
+        <AnimatedComponent>
+          <footer className="flex flex-col md:flex-row gap-8">
+            <CounterItem
+              count="2K"
+              text={t("pages.home.whoWeAre.counterItem1")}
+            />
+            <CounterItem
+              count="17M"
+              text={t("pages.home.whoWeAre.counterItem2")}
+            />
+            <CounterItem
+              count="18K"
+              text={t("pages.home.whoWeAre.counterItem3")}
+            />
+          </footer>
+        </AnimatedComponent>
       </article>
       <figure className="order-1 md:order-2 w-2/3 md:w-80 lg:w-96 mx-auto md:mr-0">
-        <img
-          className={`w-full object-cover custom-drop-shadow ${
-            isDarkMode ? "dark__drop-shadow" : ""
-          }`}
-          src={WhoWeAreImg}
-          alt="Un icono de una persona con gafas y sombrero, frente a una notebook."
-          width={564}
-          height={631}
-          loading="lazy"
-        />
+        <AnimatedComponent animation="slide" direction="left">
+          <img
+            className={`w-full object-cover custom-drop-shadow ${
+              isDarkMode ? "dark__drop-shadow" : ""
+            }`}
+            src={WhoWeAreImg}
+            alt="Un icono de una persona con gafas y sombrero, frente a una notebook."
+            width={564}
+            height={631}
+            loading="lazy"
+          />
+        </AnimatedComponent>
       </figure>
     </section>
   );
