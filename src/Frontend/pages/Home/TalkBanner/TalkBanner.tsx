777aa0fe34734dccd0d25c5158fc7535ef887b75
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-04-11 11:21:48 -0300

    feat: animaciones agregadas a Testimonials y TalkBanner

--- src/Frontend/pages/Home/TalkBanner/TalkBanner.tsx
+++ src/Frontend/pages/Home/TalkBanner/TalkBanner.tsx
@@ -2,36 +2,39 @@ import { Link } from "react-router-dom";
 import TalkBannerImg from "../../../../assets/talk-banner.avif";
 import { useDarkMode } from "../../../hooks/useDarkMode";
 import { useTranslation } from "react-i18next";
+import AnimatedComponent from "../../../components/AnimatedComponent/AnimatedComponent";
 
 function TalkBanner() {
   const { isDarkMode } = useDarkMode();
   const { t } = useTranslation();
 
   return (
-    <section className="container talk-banner py-12" id="talk-banner">
-      <article
-        className={`box-shadow__item ${
-          isDarkMode ? "dark__box-shadow__item" : ""
-        } background__accent-purple relative p-8 flex flex-col justify-center items-center min-h-80 rounded-2xl overflow-hidden`}
-      >
-        <h2 className="text-center text-3xl lg:text-5xl text-white font-bold w-5/6 mb-6 z-10">
-          {t("pages.home.talkBanner.title")}
-        </h2>
-        <Link
-          to="/login"
-          className="btn__primary btn__lime text-black shadow-lg shadow-black/30 z-10"
+    <section className="container talk-banner py-14" id="talk-banner">
+      <AnimatedComponent animation="slide" direction="up">
+        <article
+          className={`box-shadow__item ${
+            isDarkMode ? "dark__box-shadow__item" : ""
+          } background__accent-purple relative p-8 flex flex-col justify-center items-center min-h-80 rounded-2xl overflow-hidden`}
         >
-          {t("pages.home.talkBanner.button")}
-        </Link>
-        <img
-          className="absolute w-full h-full object-cover"
-          src={TalkBannerImg}
-          alt="Fondo de imagen, puntos dispersos en líneas onduladas."
-          width={1110}
-          height={324}
-          loading="lazy"
-        />
-      </article>
+          <h2 className="text-center text-3xl lg:text-5xl text-white font-bold w-5/6 mb-6 z-10">
+            {t("pages.home.talkBanner.title")}
+          </h2>
+          <Link
+            to="/login"
+            className="btn__primary btn__lime text-black shadow-lg shadow-black/30 z-10"
+          >
+            {t("pages.home.talkBanner.button")}
+          </Link>
+          <img
+            className="absolute w-full h-full object-cover"
+            src={TalkBannerImg}
+            alt="Fondo de imagen, puntos dispersos en líneas onduladas."
+            width={1110}
+            height={324}
+            loading="lazy"
+          />
+        </article>
+      </AnimatedComponent>
     </section>
   );
 }
--- src/Frontend/pages/Home/Testimonials/Testimonials.tsx
+++ src/Frontend/pages/Home/Testimonials/Testimonials.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import QuoteImg from "../../../../assets/quote-img.avif";
 import { testimonials } from "../../../mocks/testimonials";
 import { Carousel } from "./Carousel";
+import AnimatedComponent from "../../../components/AnimatedComponent/AnimatedComponent";
 
 function Testimonials() {
   const { t } = useTranslation();
@@ -11,12 +12,14 @@ function Testimonials() {
       className="testimonials relative background__accent-lime py-16"
       id="testimonials"
     >
-      <article>
-        <h2 className="text-center font-semibold text-lg md:text-2xl mb-8 text-neutral-700 px-6">
-          {t("pages.home.testimonials.title")}
-        </h2>
-        <Carousel testimonials={testimonials} />
-      </article>
+      <AnimatedComponent>
+        <article>
+          <h2 className="text-center font-semibold text-lg md:text-2xl mb-8 text-neutral-700 px-6">
+            {t("pages.home.testimonials.title")}
+          </h2>
+          <Carousel testimonials={testimonials} />
+        </article>
+      </AnimatedComponent>
       <img
         className="absolute bottom-0 right-0 w-48 md:w-auto"
         src={QuoteImg}
