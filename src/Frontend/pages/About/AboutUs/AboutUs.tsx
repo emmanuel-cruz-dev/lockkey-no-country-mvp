ff6f12dfeaa54a67a4c7f6c06a9aa00c8cd85ad2
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-04-11 11:31:22 -0300

    feat: animaciones agregadas a AboutUs y OurTeam

--- src/Frontend/pages/About/AboutUs/AboutUs.tsx
+++ src/Frontend/pages/About/AboutUs/AboutUs.tsx
@@ -4,6 +4,7 @@ import AboutImg2 from "../../../../assets/about-page-img2.webp";
 import AboutImg3 from "../../../../assets/about-page-img3.webp";
 import CheckIcon from "../../../components/CheckIcon/CheckIcon";
 import { useDarkMode } from "../../../hooks/useDarkMode";
+import AnimatedComponent from "../../../components/AnimatedComponent/AnimatedComponent";
 
 function AboutUs() {
   const { isDarkMode } = useDarkMode();
@@ -12,92 +13,98 @@ function AboutUs() {
   return (
     <section className="container">
       <article className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center py-8">
-        <article className="grid grid-cols-2 gap-8 mx-auto">
-          <img
-            className={`col-span-2 rounded-xl overflow-hidden box-shadow__item ${
-              isDarkMode ? "dark__box-shadow__item" : ""
-            }`}
-            src={AboutImg1}
-            alt="Una mujer sosteniendo un celular frente a una notebook."
-            width={540}
-            height={335}
-          />
-          <img
-            className={`hidden md:block rounded-xl overflow-hidden box-shadow__item ${
-              isDarkMode ? "dark__box-shadow__item" : ""
-            }`}
-            src={AboutImg2}
-            alt="Una mujer vestida de traje sosteniendo un celular que le ilumina la cara."
-            width={255}
-            height={251}
-          />
-          <img
-            className={`hidden md:block rounded-xl overflow-hidden box-shadow__item ${
-              isDarkMode ? "dark__box-shadow__item" : ""
-            }`}
-            src={AboutImg3}
-            alt="Una chica con una campera con la capucha puesta, sosteniendo una notebook."
-            width={255}
-            height={251}
-          />
-        </article>
+        <AnimatedComponent animation="slide" direction="right">
+          <article className="grid grid-cols-2 gap-8 mx-auto">
+            <img
+              className={`col-span-2 rounded-xl overflow-hidden box-shadow__item ${
+                isDarkMode ? "dark__box-shadow__item" : ""
+              }`}
+              src={AboutImg1}
+              alt="Una mujer sosteniendo un celular frente a una notebook."
+              width={540}
+              height={335}
+            />
+            <img
+              className={`hidden md:block rounded-xl overflow-hidden box-shadow__item ${
+                isDarkMode ? "dark__box-shadow__item" : ""
+              }`}
+              src={AboutImg2}
+              alt="Una mujer vestida de traje sosteniendo un celular que le ilumina la cara."
+              width={255}
+              height={251}
+            />
+            <img
+              className={`hidden md:block rounded-xl overflow-hidden box-shadow__item ${
+                isDarkMode ? "dark__box-shadow__item" : ""
+              }`}
+              src={AboutImg3}
+              alt="Una chica con una campera con la capucha puesta, sosteniendo una notebook."
+              width={255}
+              height={251}
+            />
+          </article>
+        </AnimatedComponent>
         <aside>
-          <h1
-            className={`${
-              isDarkMode ? "text-white/80" : "text-neutral-800"
-            } text-lg uppercase mb-2`}
-          >
-            {t("pages.about.aboutUs.title")}
-          </h1>
-          <h2 className="text-3xl lg:text-4xl xl:text-5xl font-bold mb-5">
-            {t("pages.about.aboutUs.subtitle")}
-          </h2>
-          <div
-            className={`text-lg ${
-              isDarkMode ? "text-white/70" : "text-neutral-600"
-            } mb-8`}
-          >
-            <p className="mb-2">{t("pages.about.aboutUs.paragraph1")}</p>
-            <p>{t("pages.about.aboutUs.paragraph2")}</p>
-          </div>
-          <ul
-            className={`grid grid-cols-1 sm:grid-cols-2 [&>li]:flex [&>li]:gap-3 [&>li]:mb-6 ${
-              isDarkMode ? "text-white/60" : "text-neutral-600"
-            }`}
-          >
-            <li>
-              <CheckIcon />
-              {t("pages.about.aboutUs.itemsList.item1")}
-            </li>
-            <li>
-              <CheckIcon />
-              {t("pages.about.aboutUs.itemsList.item2")}
-            </li>
-            <li>
-              <CheckIcon />
-              {t("pages.about.aboutUs.itemsList.item3")}
-            </li>
-            <li>
-              <CheckIcon />
-              {t("pages.about.aboutUs.itemsList.item4")}
-            </li>
-            <li>
-              <CheckIcon />
-              {t("pages.about.aboutUs.itemsList.item5")}
-            </li>
-            <li>
-              <CheckIcon />
-              {t("pages.about.aboutUs.itemsList.item6")}
-            </li>
-            <li>
-              <CheckIcon />
-              {t("pages.about.aboutUs.itemsList.item7")}
-            </li>
-            <li>
-              <CheckIcon />
-              {t("pages.about.aboutUs.itemsList.item8")}
-            </li>
-          </ul>
+          <AnimatedComponent animation="slide" direction="left">
+            <h1
+              className={`${
+                isDarkMode ? "text-white/80" : "text-neutral-800"
+              } text-lg uppercase mb-2`}
+            >
+              {t("pages.about.aboutUs.title")}
+            </h1>
+            <h2 className="text-3xl lg:text-4xl xl:text-5xl font-bold mb-5">
+              {t("pages.about.aboutUs.subtitle")}
+            </h2>
+            <div
+              className={`text-lg ${
+                isDarkMode ? "text-white/70" : "text-neutral-600"
+              } mb-8`}
+            >
+              <p className="mb-2">{t("pages.about.aboutUs.paragraph1")}</p>
+              <p>{t("pages.about.aboutUs.paragraph2")}</p>
+            </div>
+          </AnimatedComponent>
+          <AnimatedComponent delay={2}>
+            <ul
+              className={`grid grid-cols-1 sm:grid-cols-2 [&>li]:flex [&>li]:gap-3 [&>li]:mb-6 ${
+                isDarkMode ? "text-white/60" : "text-neutral-600"
+              }`}
+            >
+              <li>
+                <CheckIcon />
+                {t("pages.about.aboutUs.itemsList.item1")}
+              </li>
+              <li>
+                <CheckIcon />
+                {t("pages.about.aboutUs.itemsList.item2")}
+              </li>
+              <li>
+                <CheckIcon />
+                {t("pages.about.aboutUs.itemsList.item3")}
+              </li>
+              <li>
+                <CheckIcon />
+                {t("pages.about.aboutUs.itemsList.item4")}
+              </li>
+              <li>
+                <CheckIcon />
+                {t("pages.about.aboutUs.itemsList.item5")}
+              </li>
+              <li>
+                <CheckIcon />
+                {t("pages.about.aboutUs.itemsList.item6")}
+              </li>
+              <li>
+                <CheckIcon />
+                {t("pages.about.aboutUs.itemsList.item7")}
+              </li>
+              <li>
+                <CheckIcon />
+                {t("pages.about.aboutUs.itemsList.item8")}
+              </li>
+            </ul>
+          </AnimatedComponent>
         </aside>
       </article>
     </section>
--- src/Frontend/pages/About/OurTeam/OurTeam.tsx
+++ src/Frontend/pages/About/OurTeam/OurTeam.tsx
@@ -2,6 +2,7 @@ import { TeamCard } from "./TeamCard";
 import { teamMembers } from "../../../mocks/teamMembers";
 import { useDarkMode } from "../../../hooks/useDarkMode";
 import { useTranslation } from "react-i18next";
+import AnimatedComponent from "../../../components/AnimatedComponent/AnimatedComponent";
 
 function OurTeam() {
   const { isDarkMode } = useDarkMode();
@@ -9,32 +10,36 @@ function OurTeam() {
 
   return (
     <section className="container">
-      <article className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center py-8">
+      <article className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center py-12">
         <article>
-          <h1
-            className={`${
-              isDarkMode ? "text-white/80" : "text-neutral-800"
-            } text-lg uppercase mb-2`}
-          >
-            {t("pages.about.ourTeam.title")}
-          </h1>
-          <h2 className="text-3xl lg:text-4xl xl:text-5xl font-bold mb-5">
-            {t("pages.about.ourTeam.subtitle")}
-          </h2>
-          <div
-            className={`text-lg ${
-              isDarkMode ? "text-white/70" : "text-neutral-600"
-            }`}
-          >
-            <p className="mb-4">{t("pages.about.ourTeam.paragraph1")}</p>
-            <p className="mb-4">{t("pages.about.ourTeam.paragraph2")}</p>
-          </div>
+          <AnimatedComponent animation="slide" direction="left">
+            <h2
+              className={`${
+                isDarkMode ? "text-white/80" : "text-neutral-800"
+              } text-lg uppercase mb-2`}
+            >
+              {t("pages.about.ourTeam.title")}
+            </h2>
+            <h3 className="text-3xl lg:text-4xl xl:text-5xl font-bold mb-5">
+              {t("pages.about.ourTeam.subtitle")}
+            </h3>
+            <div
+              className={`text-lg ${
+                isDarkMode ? "text-white/70" : "text-neutral-600"
+              }`}
+            >
+              <p className="mb-4">{t("pages.about.ourTeam.paragraph1")}</p>
+              <p className="mb-4">{t("pages.about.ourTeam.paragraph2")}</p>
+            </div>
+          </AnimatedComponent>
         </article>
-        <aside className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4 mx-auto">
-          {teamMembers.map((member) => {
-            return <TeamCard key={member.id} {...member} />;
-          })}
-        </aside>
+        <AnimatedComponent animation="slide" direction="right">
+          <aside className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4 mx-auto">
+            {teamMembers.map((member) => {
+              return <TeamCard key={member.id} {...member} />;
+            })}
+          </aside>
+        </AnimatedComponent>
       </article>
     </section>
   );
